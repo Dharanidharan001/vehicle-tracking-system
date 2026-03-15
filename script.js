8eafe8acd16ef9ea896535da76c4b47c5ce5a4ff
dharanidharan <140431872+Dharanidharan001@users.noreply.github.com>
2026-03-15 18:44:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+var selectedBus="";
+
+function login(){
+
+var user=document.getElementById("username").value;
+var pass=document.getElementById("password").value;
+
+if(user=="admin" && pass=="1234"){
+window.location="index.html";
+}
+else{
+alert("Invalid Login");
+}
+
+}
+
+function trackBus(busName){
+
+selectedBus=busName;
+
+var img=document.getElementById("busImage");
+var btn=document.getElementById("trackBtn");
+
+if(busName=="KS"){
+img.src="ks.jpg";
+}
+
+else if(busName=="SNB"){
+img.src="snb.jpg";
+}
+
+else if(busName=="RVT"){
+img.src="rvt.jpg";
+}
+
+else if(busName=="SVT"){
+img.src="svt.jpg";
+}
+
+btn.style.display="inline-block";
+
+}
+
+function openMap(){
+
+window.open("https://www.google.com/maps/dir/Mettur/Mecheri/Omalur/Salem");
+
+}
+
+function logout(){
+
+window.location="login.html";
+
+}
